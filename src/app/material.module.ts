8e15ab6baf14fe85ec8e6c079b634bf526a9b5ef
Jonathan <tlc.jooker@gmail.com>
2019-01-15 17:51:13 -0200

    Finalizando ex1

---
+++ src/app/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MAT_DATE_LOCALE,
+  MatCheckboxModule
+ } from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatCheckboxModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-br' }
+  ]
+})
+export class MaterialModule {}
