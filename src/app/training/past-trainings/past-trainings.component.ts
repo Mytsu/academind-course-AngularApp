6b018eda4d5029c584051a242d5f11cefdb5ac06
Jonathan <tlc.jooker@gmail.com>
2019-01-17 17:18:40 -0200

    fixing mat-table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
+
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -9,17 +10,12 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./past-trainings.component.scss']
 })
 export class PastTrainingsComponent implements OnInit {
-
-  displayedColumns = [
-    'date', 'name', 'duration', 'calories', 'state'
-  ];
-
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.Exercises;
   }
-
 }
